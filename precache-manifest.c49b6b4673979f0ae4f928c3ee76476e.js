0f427b4a78c4f39d0300662066515652ef18090b
stamic1 <bhatgaurav96@gmail.com>
2020-07-26 20:34:48 +0530

    Updates

---
+++ precache-manifest.c49b6b4673979f0ae4f928c3ee76476e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd7010732d69d8a11af97f2e8d2c1be",
+    "url": "/RestaurantAdminConsole/index.html"
+  },
+  {
+    "revision": "346ca4da1ddaf22b7a38",
+    "url": "/RestaurantAdminConsole/static/css/2.e0e21483.chunk.css"
+  },
+  {
+    "revision": "a5a4f9a7029c9bb5bc62",
+    "url": "/RestaurantAdminConsole/static/css/main.694af7b5.chunk.css"
+  },
+  {
+    "revision": "346ca4da1ddaf22b7a38",
+    "url": "/RestaurantAdminConsole/static/js/2.fa2a6415.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RestaurantAdminConsole/static/js/2.fa2a6415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a4f9a7029c9bb5bc62",
+    "url": "/RestaurantAdminConsole/static/js/main.02c5bcd2.chunk.js"
+  },
+  {
+    "revision": "ac0c26de745eca7f0842",
+    "url": "/RestaurantAdminConsole/static/js/runtime-main.44b8939e.js"
+  }
+]);
